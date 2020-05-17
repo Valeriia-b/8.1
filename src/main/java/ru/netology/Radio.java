ff54f3b31fa7a455a8f99c10439e651f76cc4b9e
Valeriia <lera-bezborodova@mail.ru>
2020-05-17 13:27:45 +0300

    added lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Radio {
     private int currentStation;
     private int maxStation = 10;
@@ -7,10 +14,6 @@ public class Radio {
     private int volume;
     private int maxVolume = 100;
     private int minVolume;
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
 
     public void setCurrentStation(int currentStation) {
         if (currentStation > maxStation) {
@@ -22,27 +25,7 @@ public class Radio {
         this.currentStation = currentStation;
     }
 
-    public int getMaxStation() {
-        return maxStation;
-    }
-
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-
-    public int getMinStation() {
-        return minStation;
-    }
-
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
+     public void setVolume(int volume) {
         if (volume > maxVolume) {
             return;
         }
@@ -50,22 +33,6 @@ public class Radio {
             return;
         }
         this.volume = volume;
-    }
-
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-
-    public int getMinVolume() {
-        return minVolume;
-    }
-
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
     }
 
     public void increaseStation() {
